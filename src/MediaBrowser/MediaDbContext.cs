cfeb2ef1145f1515e62c3acf0c229eaeee20910c
Cy Scott <cy.a.scott@live.com>
2025-10-04 23:06:51 -0400

    feat: adding media query

--- src/MediaBrowser/MediaDbContext.cs
+++ src/MediaBrowser/MediaDbContext.cs
@@ -6,6 +6,48 @@ public class MediaDbContext(DbContextOptions<MediaDbContext> options) : DbContex
     public DbSet<DirectorEntity> Directors { get; init; }
     public DbSet<GenreEntity> Genres { get; init; }
     public DbSet<MediaEntity> Media { get; init; }
+
+    public IQueryable<MediaJoin> MediaJoin => Media
+        .GroupJoin(Casts, it => it.Id, it => it.MediaId,
+            (it, group) => new MediaJoin
+            {
+                Media = it,
+                Cast = group.Select(x => x.Name).ToArray()
+            })
+        .GroupJoin(Directors, it => it.Media.Id, it => it.MediaId,
+            (it, group) => new MediaJoin
+            {
+                Media = it.Media,
+                Cast = it.Cast,
+                Directors = group.Select(x => x.Name).ToArray()
+            })
+        .GroupJoin(Genres, it => it.Media.Id, it => it.MediaId,
+            (it, group) => new MediaJoin
+            {
+                Media = it.Media,
+                Cast = it.Cast,
+                Directors = it.Directors,
+                Genres = group.Select(x => x.Name).ToArray()
+            })
+        .GroupJoin(Producers, it => it.Media.Id, it => it.MediaId,
+            (it, group) => new MediaJoin
+            {
+                Media = it.Media,
+                Cast = it.Cast,
+                Directors = it.Directors,
+                Genres = it.Genres,
+                Producers = group.Select(x => x.Name).ToArray()
+            })
+        .GroupJoin(Writers, it => it.Media.Id, it => it.MediaId,
+            (it, group) => new MediaJoin
+            {
+                Media = it.Media,
+                Cast = it.Cast,
+                Directors = it.Directors,
+                Genres = it.Genres,
+                Producers = it.Producers,
+                Writers = group.Select(x => x.Name).ToArray()
+            });
     public DbSet<ProducerEntity> Producers { get; init; }
     public DbSet<WriterEntity> Writers { get; init; }
     public DbSet<UserEntity> Users { get; init; }
