3e0ca2136013a9ad8c425f6c3fc42c606eca5415
Cy Scott <cy.a.scott@live.com>
2026-03-17 21:43:13 -0400

    feat: adding schema migration

---
+++ src/Migrations/MediaBrowser.MySql/Migrations/20260316031943_Chapters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaBrowser.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chapters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "parent_id",
+                table: "media",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AddColumn<double>(
+                name: "start",
+                table: "media",
+                type: "double",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "parent_id",
+                table: "media");
+
+            migrationBuilder.DropColumn(
+                name: "start",
+                table: "media");
+        }
+    }
+}
---
+++ src/Migrations/MediaBrowser.Postgres/Migrations/20260316031955_Chapters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaBrowser.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chapters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "parent_id",
+                table: "media",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "start",
+                table: "media",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "parent_id",
+                table: "media");
+
+            migrationBuilder.DropColumn(
+                name: "start",
+                table: "media");
+        }
+    }
+}
